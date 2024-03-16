d7bee1aaab196f74a838287c199270f967b67490
David Kromka <46538919+DKromka@users.noreply.github.com>
2024-03-15 23:30:18 -0700

    added components
    added editTextviews, TextViews and Button

--- App/app/src/main/java/com/example/messenger_app/MainActivity.java
+++ App/app/src/main/java/com/example/messenger_app/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.messenger_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText IP_Address;
+    EditText Port;
+    TextView Connection;
 
+    String numIP, numPort;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected  void onResume(){
+        super.onResume();
+        init();
+    }
+    private void init(){
+        IP_Address.findViewById(R.id.edit_IP);
+        Port.findViewById(R.id.edit_Port);
+        Connection.findViewById(R.id.textView_Connection);
     }
 }
